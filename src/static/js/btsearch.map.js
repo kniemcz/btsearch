2952fa3e856f5d3c33725359ef1e07e5efb720ff
kniemcz <64684840+kniemcz@users.noreply.github.com>
2026-05-17 01:23:14 +0200

    Update btsearch.map.js
    openstreetmap support

--- src/static/js/btsearch.map.js
+++ src/static/js/btsearch.map.js
@@ -15,7 +15,8 @@ var core = {
         center: new google.maps.LatLng(52.069245, 19.480193),
         streetViewControl: false,
         scaleControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        //mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeId: 'OSM',
         mapTypeControlOptions: {
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
             position: google.maps.ControlPosition.TOP_RIGHT
@@ -30,7 +31,7 @@ var core = {
     initParams: null,
     map: null,
     infoWindow: null,
-    geocoder: null,
+    geocoder: null, // Pozostawione dla kompatybilności wstecznej, choć nieużywane przez Nominatim
     markers: null,
     selectedMarker: null,
     defaultMarkerIcon: "http://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=|ff776b|000000",
@@ -38,10 +39,21 @@ var core = {
 
     init: function(mapCanvas, initParams) {
         this.map = new google.maps.Map(mapCanvas, this.mapParams);
+        this.map.mapTypes.set("OSM", new google.maps.ImageMapType({
+            getTileUrl: function (coord, zoom) {
+                var tilesPerGlobe = 1 << zoom;
+                var x = coord.x % tilesPerGlobe;
+                if (x < 0) {
+                    x = tilesPerGlobe + x;
+                }
+                return "https://tile.openstreetmap.org/" + zoom + "/" + x + "/" + coord.y + ".png";
+            }, tileSize: new google.maps.Size(256, 256), name: "OpenStreetMap", maxZoom: 18
+        }))
         this.geocoder = new google.maps.Geocoder();
         this.markers = [];
         this.initParams = initParams;
         this.initMapBounds();
+        this.addOSMAttribution();
         ui.createMapControls(this.map);
     },
 
@@ -65,8 +77,20 @@ var core = {
         }
     },
 
+    addOSMAttribution: function() {
+        var attributionDiv = document.createElement('div');
+        attributionDiv.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+        attributionDiv.style.fontSize = '11px';
+        attributionDiv.style.fontFamily = 'Arial, sans-serif';
+        attributionDiv.style.padding = '2px 4px';
+        attributionDiv.style.margin = '0';
+        attributionDiv.style.color = '#000';
+        attributionDiv.innerHTML = '© <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors';
+
+        this.map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(attributionDiv);
+    },
+
     userLocationAutodetect: function() {
-        // Reference: https://developers.google.com/maps/articles/geolocation
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 var detectedLatLng = new google.maps.LatLng(
@@ -77,7 +101,6 @@ var core = {
                     new google.maps.LatLng(49.253465,13.710938),
                     new google.maps.LatLng(55.065787,24.268799)
                 );
-                // Only pan to auto-detected location when it is within Poland
                 if (boundsOfPoland.contains(detectedLatLng)) {
                     core.map.setCenter(detectedLatLng);
                     core.map.setZoom(15);
@@ -116,33 +139,26 @@ var core = {
     },
 
     clearMarkers: function() {
-        // Clear selected marker if it is out of current map bounds
         if (this.selectedMarker !== null &&
             this.map.getBounds().contains(this.selectedMarker.getPosition()) === false) {
             this.clearSelectedMarker();
         }
 
-        // Clear all markers but not selected one
         for (var i in this.markers) {
             if (this.markers[i] != this.selectedMarker) {
                 this.markers[i].setMap(null);
             }
         }
-
-        // Reset markers array
         this.markers = [];
     },
 
     createMarker: function(latlng, icon) {
-        // Reuse selected marker if possible instead of creating a new one
-        // (this is mainly to preserve opened infoWindow over selected marker)
         if (this.selectedMarker !== null &&
             this.selectedMarker.getPosition().equals(latlng)) {
             this.markers.push(this.selectedMarker);
             return this.selectedMarker;
         }
 
-        // Create new marker
         marker = new google.maps.Marker({
             position: latlng,
             icon: icon,
@@ -152,10 +168,6 @@ var core = {
         return marker;
     },
 
-    /**
-     * Wrapper function around remote request to load locations for given
-     * map bounds and filters.
-     */
     loadLocations: function() {
         mapStatus.wait();
         if (this.map.getZoom() >= 11) {
@@ -170,15 +182,6 @@ var core = {
         mapStatus.updateAll();
     },
 
-    /**
-     * Method executed on map panning/zooming, can be overriden
-     *
-     * Parameter 'locations' is json object containing a list
-     * where each record should have following fields:
-     * id, latitude, longitude, icon
-     *
-     * @param locations
-     */
     displayLocations: function(data) {
         this.clearMarkers();
         locations = data.objects;
@@ -193,18 +196,8 @@ var core = {
         mapStatus.waitDone();
     },
 
-    /**
-     * Method displaying clicked location details, can be overriden
-     *
-     * Parameter 'locationData' is json object containing
-     * just 'html' field with pre-rendered info window content.
-     *
-     * @param locationData
-     * @param marker
-     */
     displayLocationInfo: function(locationData, marker) {
         this.clearInfoWindow();
-        // this.clearSelectedMarker(); -- don't make the marker disappear
         this.infoWindow = new google.maps.InfoWindow({
             content: locationData.info
         });
@@ -213,35 +206,56 @@ var core = {
         events.infoWindowClose(this.infoWindow);
     },
 
-    // @TODO: request.searchLocation -> callback: ui.displaySearchResult (?)
+    // Zmieniona metoda wyszukiwania na Nominatim (OpenStreetMap)
     searchLocation: function(query) {
-        $('#control-panel-search-results').html('');
-        params = {
-            'address': query,
-            'region': 'pl'
+        $('#control-panel-search-results').html('Szukanie...');
+        
+        var self = this;
+        var url = "https://nominatim.openstreetmap.org/search";
+        
+        var params = {
+            q: query,
+            format: 'json',
+            addressdetails: 1,
+            countrycodes: 'pl',
+            limit: 10
         };
-        this.geocoder.geocode(params, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                core.displaySearchResult(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-                if (results.length > 1) {
-                    // Generate results list
-                    var resultsHtml = '<ul>';
-                    for (var i in results) {
-                        resultsHtml += '<li><a href="javascript:void(0);" class="search-result-item" data-latitude="' + results[i].geometry.location.lat() + '" data-longitude="' + results[i].geometry.location.lng() + '">' + results[i].formatted_address + '</a></li>';
+
+        $.ajax({
+            url: url,
+            data: params,
+            dataType: 'json',
+            success: function(results) {
+                if (results && results.length > 0) {
+                    var first = results[0];
+                    self.displaySearchResult(parseFloat(first.lat), parseFloat(first.lon));
+
+                    if (results.length > 1) {
+                        var resultsHtml = '<ul>';
+                        for (var i in results) {
+                            var res = results[i];
+                            resultsHtml += '<li><a href="javascript:void(0);" class="search-result-item" ' +
+                                'data-latitude="' + res.lat + '" ' +
+                                'data-longitude="' + res.lon + '">' + 
+                                res.display_name + '</a></li>';
+                        }
+                        resultsHtml += '</ul>';
+                        $('#control-panel-search-results').html(resultsHtml);
+
+                        ui.activatePanel($('#control-panel-search-results'), 'Rezultat wyszukiwania');
+
+                        $('.search-result-item').on('click', function(){
+                            self.displaySearchResult($(this).data('latitude'), $(this).data('longitude'));
+                        });
+                    } else {
+                        $('#control-panel-search-results').html('');
                     }
-                    resultsHtml += '</ul>';
-                    $('#control-panel-search-results').html(resultsHtml);
-
-                    // Display search results panel
-                    ui.activatePanel($('#control-panel-search-results'), 'Rezultat wyszukiwania');
-
-                    // Attach event to search results
-                    $('.search-result-item').on('click', function(){
-                        core.displaySearchResult($(this).data('latitude'), $(this).data('longitude'));
-                    });
+                } else {
+                    $('#control-panel-search-results').html('Nie znaleziono wyników.');
                 }
-            } else {
-                $('#control-panel-search-results').html(status);
+            },
+            error: function() {
+                $('#control-panel-search-results').html('Błąd serwera wyszukiwania.');
             }
         });
     },
@@ -254,7 +268,6 @@ var core = {
     }
 
 };
-
 
 /**
  * Object to handle map events
@@ -359,7 +372,6 @@ var distanceService = {
             this.polyline.setMap(core.map);
             this.distance = google.maps.geometry.spherical.computeLength(path);
             this.heading = google.maps.geometry.spherical.computeHeading(path[0], path[1]);
-            // this.heading = utils.calculateHeading(path[0], path[1]);
         }
     }
 };
@@ -398,40 +410,12 @@ var requests = {
         });
     },
 
-    /* Instead of loading location info in panel, open it in fancybox
-    getBaseStationInfo: function(baseStationId) {
-        $.ajax({
-            url: "/get_base_station_info/" + baseStationId + "/",
-            success: function(data) {
-                $('#control-panel-location-info').html(data);
-                ui.activatePanel($('#control-panel-location-info'), 'Szczegóły lokalizacji');
-            }
-        });
-    },
-
-    getUkeLocationInfo: function(ukeLocationId, networkCode) {
-        $.ajax({
-            url: "/get_uke_location_info/" + ukeLocationId + "," + networkCode + "/",
-            success: function(data) {
-                $('#control-panel-location-info').html(data);
-                ui.activatePanel($('#control-panel-location-info'), 'Szczegóły lokalizacji');
-            }
-        });
-    },
-    */
-
     setControlPanelContent: function(panel) {
         $.ajax({
             url: "/map/ui/control_panel/",
             success: function(data) {
                 panel.innerHTML = data;
 
-                // Wait for panel elements to attach to DOM properly
-                // before attaching events to them
-                // Try jQuery's .on event? See:
-                // https://github.com/zoyalab/zoyalab.com/blob/master/js/main.js#L20
-                //
-                // TODO: Using setInterval certainly smells shit. Fix it.
                 inv = setInterval(function(){
                     if (ui.ready()) {
                         core.bindMapEvents();
@@ -473,11 +457,6 @@ var ui = {
         return (typeof(window.onpopstate) != 'undefined');
     },
 
-    /**
-     * Check if dynamically created elements are properly rendered
-     *
-     * @returns {Boolean}
-     */
     ready: function() {
         uiElementsToCheck = ['control-panel-container',
                              'status-panel-container',
@@ -522,7 +501,6 @@ var ui = {
     },
 
     createAdPanel: function(map) {
-        // Reference: https://developers.google.com/maps/documentation/javascript/advertising
         var adUnitDiv = document.createElement('div');
         adUnitDiv.id = 'googlead-panel-container';
         var adUnitOptions = {
@@ -536,7 +514,6 @@ var ui = {
     },
 
     bindControlPanelEvents: function() {
-        // Search form submission
         $('#search-form').submit(function(){
             query = $('#search-box').val();
             if (query !== '') {
@@ -546,17 +523,14 @@ var ui = {
             return false;
         });
 
-        // Network filter dropdown change
         $('#network-filter').change(function(){
             ui.resetMap();
         });
 
-        // Network standards filter
         $('.standard-filter').click(function(){
             ui.resetMap();
         });
 
-        // Network standards filter
         $('.band-filter').click(function(){
             ui.resetMap();
         });
@@ -565,7 +539,6 @@ var ui = {
             ui.resetMap();
         });
 
-        // Data source filter
         $('input[name=data-source]').change(function(){
             ui.resetMap();
             if ($(this).val() == 'locations') {
@@ -583,10 +556,7 @@ var ui = {
 
         $('a#data-source-help-icon').fancybox();
 
-        // Search box autocomplete
-        searchBox = document.getElementById('search-box');
-        autocomplete = new google.maps.places.Autocomplete(searchBox);
-        autocomplete.bindTo('bounds', core.map);
+        // Wyłączono Google Places Autocomplete dla spójności z Nominatim
     },
 
     activatePanel: function(panelObject, panelTitle) {
@@ -612,8 +582,6 @@ var ui = {
     toggleControlPanel: function() {
         $('#control-panel-body').toggle({
             complete: function() {
-                // This little hack is needed to maintain scrollability of the
-                // control panel when the viewport's height is too low
                 var visible = $(this).is(':visible');
                 if (!visible && $('#control-panel-container').css('bottom') == '0px') {
                     $('#control-panel-container').css('bottom', 'auto');
@@ -697,7 +665,6 @@ var filters = {
     },
 
     setDataSourceFilter: function() {
-        //this.dataSource = $('#data-source-filter').val();
         this.dataSource = $('input[name=data-source]:checked').val();
     },
 
@@ -734,7 +701,7 @@ var mapStatus = {
         var lng = point.lng().toFixed(6);
         var lat2 = utils.deg2dms(lat, 'lat');
         var lng2 = utils.deg2dms(lng, 'lng');
-        return lat + '&nbsp;' + lng + '&nbsp;&hArr;&nbsp;' + lat2 + '&nbsp;' + lng2;
+        return lat + ' ' + lng + ' ⇔ ' + lat2 + ' ' + lng2;
     },
 
     updateAll: function() {
@@ -779,7 +746,7 @@ var mapStatus = {
             if (distance_ta > 63) distance_ta = 'max';
             $('#status-distance').html(distance_km);
             $('#status-ta').html(distance_ta);
-            $('#status-heading').html(heading + '&deg;');
+            $('#status-heading').html(heading + '°');
             $('#status-gps-marker').html(this.getLatLngExtendedInfo(distanceService.startPoint));
         } else {
             $('#status-panel-distance').hide();
@@ -832,9 +799,7 @@ var mapStatus = {
 };
 
 var utils = {
-    // Credits: Krzysiek Niemczyk ;)
     deg2dms: function(coordinate, latlng) {
-        // jeśli nie określono argumentu to nie wykonujemy funkcji
         if (latlng != 'lat' && latlng != 'lng') return;
 
         coordinate = Math.abs(coordinate);
@@ -845,79 +810,60 @@ var utils = {
 
         s = (s - m * 60).toFixed(2);
 
-        if (s < 10)
-            s = '0' + s;
-        if (m < 10)
-            m = '0' + m;
+        if (s < 10) s = '0' + s;
+        if (m < 10) m = '0' + m;
 
         var suffix = latlng == 'lat' ? coordinate > 0 ? 'N' : 'S' : coordinate > 0 ? 'E' : 'W';
 
-        return d + '&ordm;' + m + '\'' + s + '\'\'&nbsp;' + suffix;
+        return d + 'º' + m + '\'' + s + '\'\' ' + suffix;
     }
-
-    /*calculateHeading: function(point1, point2) {
-        var lat1 = point1.lat(); var lon1 = point1.lng();
-        var lat2 = point2.lat(); var lon2 = point2.lng();
-
-        var lat1 = lat1 * Math.PI / 180;
-        var lat2 = lat2 * Math.PI / 180;
-        var dLon = (lon2 - lon1) * Math.PI / 180;
-
-        var y = Math.sin(dLon) * Math.cos(lat2);
-        var x = Math.cos(lat1) * Math.sin(lat2) -
-        Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
-
-        var brng = Math.atan2(y, x);
-
-        return (((brng * 180 / Math.PI) + 360) % 360);
-    }*/
 };
 
+// Skróty klawiszowe
 $(document).bind('keypress', 'g', function(){
     $('#standard-filter-gsm').prop('checked', !$('#standard-filter-gsm').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', 'u', function(){
     $('#standard-filter-umts').prop('checked', !$('#standard-filter-umts').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', 'l', function(){
     $('#standard-filter-lte').prop('checked', !$('#standard-filter-lte').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', 'c', function(){
     $('#standard-filter-cdma').prop('checked', !$('#standard-filter-cdma').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', '5', function(){
     $('#standard-filter-5g').prop('checked', !$('#standard-filter-5g').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', 'i', function(){
     $('#standard-filter-iot').prop('checked', !$('#standard-filter-iot').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', '9', function(){
     $('#band-filter-900').prop('checked', !$('#band-filter-900').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', '1', function(){
     $('#band-filter-1800').prop('checked', !$('#band-filter-1800').prop('checked'));
     ui.resetMap();
 });
-
 $(document).bind('keypress', '2', function(){
     $('#band-filter-2100').prop('checked', !$('#band-filter-2100').prop('checked'));
     ui.resetMap();
 });
-
+$(document).bind('keypress', '3', function(){
+    $('#band-filter-3500').prop('checked', !$('#band-filter-3500').prop('checked'));
+    ui.resetMap();
+});
+$(document).bind('keypress', '7', function(){
+    $('#band-filter-700').prop('checked', !$('#band-filter-700').prop('checked'));
+    ui.resetMap();
+});
 $(document).bind('keypress', '8', function(){
     $('#band-filter-800').prop('checked', !$('#band-filter-800').prop('checked'));
     ui.resetMap();
